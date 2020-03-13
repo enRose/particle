2e78dd1c20efecc603a1484934f9bde8f105d93f
Yini Yin <yiniski@outlook.com>
2020-03-13 13:11:46 +1300

    change extension name to particle

--- out/extension.js
+++ out/extension.js
@@ -1,19 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloworld" is now active!');
+    console.log('particle is activated.');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
+    let disposable = vscode.commands.registerCommand('extension.particle', () => {
+        // The code placed here will be executed every time your command is executed
         vscode.languages.getLanguages()
             .then((listOfLanguages) => {
             var _a;
@@ -22,8 +18,7 @@ function activate(context) {
         // Display a message box to the user
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        vscode.window.showInformationMessage('Hello Yini!' + time);
-        vscode.window.showWarningMessage('corona');
+        vscode.window.showInformationMessage('Hello particle!' + time);
     });
     context.subscriptions.push(disposable);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
+	console.log('particle is activated.');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('extension.particle', () => {
+		// The code placed here will be executed every time your command is executed
 
 		vscode.languages.getLanguages()
             .then((listOfLanguages) => {
@@ -28,11 +24,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 		const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-		vscode.window.showInformationMessage('Hello Yini!' + time);
-
-		vscode.window.showWarningMessage('corona')
-
-		
+		vscode.window.showInformationMessage('Hello particle!' + time);		
 		
 	});
 
