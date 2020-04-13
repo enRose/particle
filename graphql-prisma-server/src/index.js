92d4587f34480fb8503726338990964e74a96e20
yiniy <yiniy@Yini-Yins-MacBook-Pro.local>
2020-04-13 22:10:49 +1200

    add a graphql prisma mongo server

---
+++ graphql-prisma-server/src/index.js
@@ -0,0 +1,43 @@
+import { GraphQLServer } from 'graphql-yoga';
+import { importSchema } from 'graphql-import';
+import jwt from 'jsonwebtoken';
+
+import resolvers from './resolvers';
+import { Prisma } from '../prisma/generated/prisma-client';
+
+import 'dotenv/config';
+
+const typeDefs = importSchema('src/schemas/user.graphql');
+
+export const db = new Prisma({
+  endpoint: process.env.PRISMA_ENDPOINT || 'http://192.168.99.100:4466',
+  secret: process.env.PRISMA_SECRET || '',
+});
+
+const getCurrentUser = async (request) => {
+    if (!request.headers.token) {
+        return null;
+    }
+    const user = await jwt.decode(
+        request.headers.token,
+        process.env.JWT_SECRET_KEY,
+    );
+    return { ...user };
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+    context: async ({ request }) => {
+        const me = await getCurrentUser(request);
+
+        return {
+          me,
+          prisma: db,
+        };
+    }
+});
+
+server.start({ port: process.env.PORT }, () => {
+    console.log('App running on http://localhost:4400');
+});
