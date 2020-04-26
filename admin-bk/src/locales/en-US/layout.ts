30036c44645ea5c8a9bb6642d6e5990de2ccddf0
yiniy <yiniy@Yini-Yins-MacBook-Pro.local>
2020-04-27 09:32:17 +1200

    remove ant-pro

--- admin-bk/src/locales/en-US/layout.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  'layout.user.logoDesc': 'Instant Park find a car park any where any time',
-  'layout.user.footerCopyRight': 'barintech',
-  'layout.user.footerCompanyName': 'barin design',
-};
--- admin-bk/src/locales/zh-CN/layout.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  'layout.user.logoDesc': '闪停 随时随地想停就停',
-  'layout.user.footerCopyRight': 'barin 技术部出品',
-  'layout.user.footerCompanyName': 'barin 设计',
-};
